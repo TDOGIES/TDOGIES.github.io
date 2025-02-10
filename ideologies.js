f29e94ab652319d8e4b8b2b7a5c6e2abaad6617d
TDOGIES <tobi_carr@hotmail.com>
2025-02-10 18:10:08 +1100

    let's see how this goes

---
+++ ideologies.js
@@ -0,0 +1,119 @@
+ideologies = [
+    {
+        "name": "Centrist",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Bunkered Away",
+        "stats": {
+            "econ": 0,
+            "dipl": 20,
+            "govt": 70,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Wanderer",
+        "stats": {
+            "econ": 100,
+            "dipl": 70,
+            "govt": 90,
+            "scty": 40
+        }
+    },
+    {
+        "name": "The People's Leader",
+        "stats": {
+            "econ": 40,
+            "dipl": 90,
+            "govt": 90,
+            "scty": 60
+        }
+    },
+    {
+        "name": "The Protector",
+        "stats": {
+            "econ": 40,
+            "dipl": 10,
+            "govt": 90,
+            "scty": 40
+        }
+    },
+    {
+        "name": "Pressing the Attack",
+        "stats": {
+            "econ": 60,
+            "dipl": 60,
+            "govt": 60,
+            "scty": 20
+        }
+    },
+    {
+        "name": "The More the Merrier",
+        "stats": {
+            "econ": 20,
+            "dipl": 80,
+            "govt": 30,
+            "scty": 60
+        }
+    },
+    {
+        "name": "Solo Anarchy",
+        "stats": {
+            "econ": 90,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Zombie Hatred",
+        "stats": {
+            "econ": 50,
+            "dipl": 60,
+            "govt": 60,
+            "scty": 0
+        }
+    },
+    {
+        "name": "Travelling Warband",
+        "stats": {
+            "econ": 70,
+            "dipl": 30,
+            "govt": 90,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Ruling Class",
+        "stats": {
+            "econ": 40,
+            "dipl": 50,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Team and Work",
+        "stats": {
+            "econ": 70,
+            "dipl": 80,
+            "govt": 70,
+            "scty": 40
+        }
+    },
+    {
+        "name": "Shelter's Layman",
+        "stats": {
+            "econ": 20,
+            "dipl": 70,
+            "govt": 50,
+            "scty": 50
+        }
+    }
+];
---
+++ questions.js
@@ -0,0 +1,462 @@
+questions = [
+    {
+        "question": "Death by people is more of a concern for me than zombies.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The less attached I am to a single group, the better.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is better for everyone to remove as many zombies as possible, no matter the cost.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Groups should share as many personal weaponry and supplies as possible.",
+        "effect": {
+            "econ": -10,
+            "dipl": -5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I’m happy for anyone of able sound and mind to join my group.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Shelters should never be open to anyone unknown, even if they are pleading.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I would rather trust a group to manage my basic supplies than organise and store it by myself.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It would be best to let the individual control when and how they go out into dangerous areas.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Those with more luxuries should fairly share it with others.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Those who are infected should leave the shelter instead of being killed.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Regardless of opinion, it is important to stay and side with the leader’s decision.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": -10,
+            "scty": 0
+        } 
+    },
+    {
+        "question": "Those with the ability to hoard more supplies should be allowed to do what they’d like with it.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "There is never a reason to fight a zombie hoard that is coming after you.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "I would not hesitate at ending a stranger’s life once I knew they were infected.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Regardless of opinion, it is important to stay and side with the majority’s decision. ",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Protection from zombies is a right all people should get.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "There is no need to use resources on killing a completely stuck zombie.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "It is important to maintain a friendly disposition to strangers.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I support extensive efforts to reclaim as much territory as possible from zombies.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Holding onto more weapons than you can carry is a waste of supply space.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        } 
+    },
+    {
+        "question": "It is important to establish claimed territory as a sovereign group.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Exploring hoarded zombie areas is a waste of time and resources.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        } 
+    },
+    {
+        "question": "Individual zombies are not hard creatures for me to get rid of.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "There is never a reason to discuss my plans with strangers, even if it negatively affects them.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A single, united group would benefit all people in the area.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Killing someone before they turn should be handled by someone close to the infected.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "A shelter's values should be followed by individuals as much as possible, or they must leave.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "People are not of value to me if they do not have any useful skills.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The restriction of basic supplies to others is necessary for long-term survival.",
+        "effect": {
+            "econ": -10,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is very important to maintain some sense of law and order with others.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Lack of defensive, bunkering tools is the greatest threat to our survival.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        } 
+    },
+    {
+        "question": "Other people will hold back my survival.",
+        "effect": {
+            "econ": 5,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The sacrifice of some people’s autonomy is necessary to protect a shelter from break-ins.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "I prefer being democratic whenever possible. ",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "If I don’t trust a stranger, I should not need to justify hurting them.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I would save a friend if I saw them trapped in a zombie hoard, alone.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "The only rule in the apocalypse is the survival of the fittest.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "I prefer having or being in leadership whenever possible. ",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The best defense is a good offense.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "I would prefer having one, versatile weapon for my survival over having multiple, niche weapons.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "My first instinct is to stay in the shelter I have and the people I end up with for as long as possible.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Attempts to bring back some form of society are a waste of time.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Maintaining a single, long-term shelter would not help me survive.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I would rather have a large, adjustable group working with me over a small, knitted group.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },    
+    {
+        "question": "Sitting inside is of no value if you are not also spending time outside, actively fighting back.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "If it meant a more secure shelter, I would stay far away from anyone else (e.g a mansion or bunker).",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        } 
+    },
+    {
+        "question": "We must do more than simply survive, we have to bring back the luxuries we lost.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "I feel the need to travel and move about, avoiding zombies and adjusting my way as I go.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "I would not mind being in a group for hire, moving about and killing zombies as needed.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "I would not stay in a group that is governed by hierarchy and 'jobs'.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+
+
+
+
+
+
+
+
+
+    
+];
